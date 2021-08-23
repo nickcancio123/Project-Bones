78b6353569433c3baf18a9707af7bf69cdd45608
nickcancio <nickcancio123@gmail.com>
2021-08-23 16:33:00 -0400

    Implemented basic game manager script
    -Spawns instantiation of player prefab

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject skelyPrefab;
+
+    void Start()
+    {
+        if (skelyPrefab)
+        {
+            Vector3 spawnPoint = new Vector3(Random.Range(-5, 5), 5, Random.Range(-5, 5));
+            PhotonNetwork.Instantiate(this.skelyPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+}
